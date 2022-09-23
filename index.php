763dfe7d2418c0e4a3135805889ca18d4552e829
Bram van Ballegooijen <99046330@mydavinci.nl>
2022-09-23 13:38:39 +0200

    added if statement if the button is true.
    added button if statement. changes in styling  and code comments

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
 <?php
 
+    // all requires //
+
     require 'pokemon.php';
     require 'Pikachu.php';
     require 'Charmeleon.php';
     require 'header.php';
 
+    // pokemon class // 
+
     $pikachu = new pikachu();
     $charmeleon = new charmeleon();
+
+    // status page // 
+
+    $status = $_GET['status'];
 ?>
 
 
@@ -20,11 +28,26 @@
                     <p class="btn btn-warning"> <?php echo $pikachu->name ?></p>
                     <p>  VS  </p>
                     <p class="btn btn-danger"> <?php echo $charmeleon->name ?></p>
-
                 </div>
 
+                    <div class='card-body'>
+                    <h2 class='card-title'> battle info </h2>
+
+                        <?php 
+                            // if button is true then it shows the battle // 
+
+                            if ($status == 'battle') {
+                                echo $pikachu->name . ' VS ' . $charmeleon->name ;
+                            }
+
+                            else{
+                            echo 'Er is nog geen data'; 
+                            }
+                        ?>
+                     </div>
+
                 <div class='text-center'>
-                    <button type="button" class="btn-lg btn-success "> Start battle </button>
+                    <button type="button" class="btn-lg btn-success" onclick = 'location.href ="index.php?status=battle"'> Start battle </button>
                 </div> 
             </div>
         </div>
